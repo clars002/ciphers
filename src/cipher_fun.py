f00a20b666e8247b8ef04bf5030243dc2ab984d3
Caelan Larsen <clars002@odu.edu>
2024-11-26 21:58:37 -0500

    renamed folders, write ciphertext to file

--- src/cipher_fun.py
+++ src/cipher_fun.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 
 def process_args():
@@ -13,7 +14,10 @@ def process_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--input", type=str, default="input/sample.txt", help="Path to the input file."
+        "--input",
+        type=str,
+        default="plaintext/sample.txt",
+        help="Path to the input file.",
     )
     parser.add_argument(
         "--mode", type=str, default="encrypt", help="Whether to encrypt or decrypt."
@@ -35,19 +39,28 @@ def main():
     args = process_args()
 
     if args.mode == "encrypt":
+        base_filename = os.path.splitext(os.path.basename(args.input))[0]
+
+        output_name = base_filename + "_encrypted.txt"
+
         with open(args.input) as file:
             plaintext = file.read()
             ciphertext = ""
 
             for char in plaintext:
                 initial_index = ord(char)
-                shifted_index = (initial_index + args.offset)
-                
+                shifted_index = initial_index + args.offset
+
                 new_letter = chr(shifted_index)
 
                 ciphertext += new_letter
 
-        print(ciphertext)
+        output_file = open(f"ciphertext/{output_name}", "w")
+
+        output_file.write(ciphertext)
+
+        output_file.close()
+
 
 if __name__ == "__main__":
     main()
