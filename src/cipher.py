5d0d9caf7e0a820f7d309d3bf6760c359efe52bd
Caelan Larsen <clars002@odu.edu>
2024-11-27 18:16:41 -0500

    add alphabetic option

--- src/cipher.py
+++ src/cipher.py
@@ -4,30 +4,178 @@ class Cipher:
 
     def decrypt(ciphertext: str):
         raise NotImplementedError
-    
+
 
 class CaesarCipher(Cipher):
     def __init__(self, alphabetic: bool = False):
         self.alphabetic = alphabetic
 
-    
     def encrypt(self, plaintext: str, key: int):
         ciphertext = ""
-        offset = key
 
-        if self.alphabetic:
-            offset = key % 26
+        offset = key
 
         for char in plaintext:
             if self.alphabetic and not char.isalpha():
                 ciphertext += char
                 continue
 
-            initial_index = ord(char)
-            shifted_index = initial_index + offset
+            if not self.alphabetic:
+                initial_index = ord(char)
+                shifted_index = initial_index + offset
 
-            new_letter = chr(shifted_index)
+                new_letter = chr(shifted_index)
 
-            ciphertext += new_letter
+                ciphertext += new_letter
+            else:
+                upper = False
+
+                if char.isupper():
+                    upper = True
+                    char = char.lower()
+
+                alpha_index = ord(char) - ord("a")
+
+                shifted_alpha_index = (alpha_index + offset) % 26
+
+                new_letter = chr(shifted_alpha_index + ord("a"))
+
+                if upper:
+                    new_letter = new_letter.upper()
+
+                ciphertext += new_letter
+
+        return ciphertext
+
+    def score_candidate(self, text: str):
+        score = 0
+
+        word_list = open("wordlist.txt", "r")
+        words = [line.strip() for line in word_list.readlines()]
+        word_set = set()
+
+        for word in words:
+            word_set.add(word)
+
+        text_length = len(text)
+
+        score = 0
+
+        for l in range(2, 45):
+            for left_border in range(text_length - l):
+                right_border = left_border + l
+
+                window_size = right_border - left_border
+
+                substring = text[left_border:right_border]
+
+                substring = substring.lower()
+
+                substring_length = len(substring)
+
+                if substring in word_set:
+                    score += substring_length**2
+                    # print(f"Wordlist hit: {substring}.")
+
+        return score
+
+    def decrypt(self, ciphertext: str, key: int = 0):
+        offset = key
+
+        if offset != 0:
+
+            plaintext = ""
+
+            if not self.alphabetic:
+
+                for char in ciphertext:
+                    initial_index = ord(char)
+                    shifted_index = initial_index - offset
+
+                    new_letter = chr(shifted_index)
+
+                    plaintext += new_letter
+
+            else:
+
+                for char in ciphertext:
+
+                    if not char.isalpha():
+                        plaintext += char
+                        continue
+
+                    upper = False
+
+                    if char.isupper():
+                        upper = True
+                        char = char.lower()
+
+                    initial_index = ord(char) - ord("a")
+                    shifted_index = (initial_index - offset) % 26
+
+                    new_letter = chr(shifted_index + ord("a"))
+
+                    if upper:
+                        new_letter = new_letter.upper()
+
+                    plaintext += new_letter
+
+            return plaintext
+
+        else:  # Try to brute force if an offset (key) is not supplied
+            best_candidate = None
+            best_score = -1
+
+            if self.alphabetic:
+                limit = 26
+            else:
+                limit = 128
+
+            for i in range(limit):
+                plaintext = ""
+
+                for char in ciphertext:
+
+                    if not self.alphabetic:
+                        initial_index = ord(char)
+                        shifted_index = initial_index - i
+
+                        if shifted_index < 0:
+                            shifted_index += 128
+
+                        new_letter = chr(shifted_index)
+
+                    else:
+                        if not char.isalpha():
+                            plaintext += char
+                            continue
+
+                        upper = False
+
+                        if char.isupper():
+                            upper = True
+                            char = char.lower()
+
+                        initial_index = ord(char) - ord("a")
+                        shifted_index = (initial_index - i) % 26
+
+                        new_letter = chr(shifted_index + ord("a"))
+
+                        if upper:
+                            new_letter = new_letter.upper()
+
+                    plaintext += new_letter
+
+                score = self.score_candidate(plaintext)
+
+                if score > best_score:
+                    best_score = score
+                    best_candidate = plaintext
+
+                    print(
+                        f'Processed {i + 1}/{limit} candidates thus far; best candidate:\n "{plaintext[:512]}..." \nWith score: {best_score}.'
+                    )
+
+            return best_candidate
 
         return ciphertext
--- src/cipher_fun.py
+++ src/cipher_fun.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 
+import cipher
+
 
 def process_args():
     """
@@ -29,114 +31,21 @@ def process_args():
         help="Which cypher to use for encryption (unused in decryption mode).",
     )
     parser.add_argument(
-        "--offset", type=int, default=8, help="Offset to shift for Caesar cipher."
+        "--key", type=int, default=8, help="Offset to shift for Caesar cipher."
     )
     parser.add_argument(
         "--alphabetic",
-        action='store_true',
-        help="Enables shifting with respect to the 26-character alphabet instead of 128-character ASCII set."
+        action="store_true",
+        help="Enables shifting with respect to the 26-character alphabet instead of 128-character ASCII set.",
     )
 
     return parser.parse_args()
 
 
-def encrypt_caesar(plaintext: str, offset: int):
-    ciphertext = ""
-
-    for char in plaintext:
-        initial_index = ord(char)
-        shifted_index = initial_index + offset
-
-        new_letter = chr(shifted_index)
-
-        ciphertext += new_letter
-
-    return ciphertext
-
-
-def score_candidate(text: str):
-    score = 0
-
-    word_list = open("wordlist.txt", "r")
-    words = [line.strip() for line in word_list.readlines()]
-    word_set = set()
-
-    for word in words:
-        word_set.add(word)
-
-    text_length = len(text)
-
-    score = 0
-
-    for l in range(2, 45):
-        for left_border in range(text_length - l):
-            right_border = left_border + l
-
-            window_size = right_border - left_border
-
-            substring = text[left_border:right_border]
-
-            substring = substring.lower()
-
-            substring_length = len(substring)
-
-            if substring in word_set:
-                score += substring_length**2
-                # print(f"Wordlist hit: {substring}.")
-
-    return score
-
-
-def decrypt_caesar(ciphertext: str, offset: int = 0):
-    if offset != 0:
-
-        plaintext = ""
-
-        for char in ciphertext:
-            initial_index = ord(char)
-            shifted_index = initial_index - offset
-
-            new_letter = chr(shifted_index)
-
-            plaintext += new_letter
-
-        return plaintext
-
-    else:  # Try to brute force if an offset (key) is not supplied
-        best_candidate = None
-        best_score = -1
-
-        for i in range(128):
-            plaintext = ""
-
-            for char in ciphertext:
-                initial_index = ord(char)
-                shifted_index = initial_index - i
-
-                if shifted_index < 0:
-                    shifted_index += 128
-
-                new_letter = chr(shifted_index)
-
-                plaintext += new_letter
-
-            score = score_candidate(plaintext)
-
-            if score > best_score:
-                best_score = score
-                best_candidate = plaintext
-
-                print(
-                    f"Current best candidate: {best_candidate} with score {best_score}"
-                )
-
-        return best_candidate
-
-    return ciphertext
-
-
 def main():
     args = process_args()
+
+    my_cipher = cipher.CaesarCipher(args.alphabetic)
 
     input_path = args.input
     base_filename = os.path.splitext(os.path.basename(input_path))[0]
@@ -147,7 +56,7 @@ def main():
         with open(args.input) as file:
             plaintext = file.read()
 
-            ciphertext = encrypt_caesar(plaintext, args.offset)
+            ciphertext = my_cipher.encrypt(plaintext, args.key)
 
         output_file = open(f"ciphertext/{output_name}", "w")
 
@@ -161,7 +70,7 @@ def main():
         with open(args.input) as file:
             ciphertext = file.read()
 
-            plaintext = decrypt_caesar(ciphertext, args.offset)
+            plaintext = my_cipher.decrypt(ciphertext, args.key)
 
         output_file = open(f"plaintext/{output_name}", "w")
 
