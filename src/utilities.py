82e2902d0b7e1d9b0948733a35adbc5c9ccecf3e
Caelan Larsen <clars002@odu.edu>
2024-11-28 10:29:53 -0500

    document utilities

--- src/utilities.py
+++ src/utilities.py
@@ -1,7 +1,15 @@
+"""
+File containing small utility operations used elsewhere in the
+code.
+"""
+
 import os
 
 
 def clear_screen():
+    """
+    Clears the terminal output.
+    """
     if os.name == "nt":
         os.system("cls")
     else:
@@ -9,5 +17,15 @@ def clear_screen():
 
 
 def preview_text(text: str):
+    """
+    Creates a "preview" version of text by placing it in quotes and
+    abridging it if longer than 512 characters.
+
+    Args:
+        text (str): Text to be previewed.
+
+    Returns:
+        str: The generated preview.
+    """
     preview = f'"{text[:512]}..."' if len(text) > 512 else f'"{text}"'
     return preview
